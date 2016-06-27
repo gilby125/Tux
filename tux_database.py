8028dd874103cb83ceabea4811c40f5715c85f49
ARCHER <gavigan.ryan@gmail.com>
2016-06-27 01:13:00 -0400

    migrate tux_database into functions; add sendEmail param

--- tux_database.py
+++ tux_database.py
@@ -1,35 +1,54 @@
 # tux_database
 # Ryan Gavigan
 # 06/26/16
+import random
 import sqlite3 as sql
 from contextlib import closing
 
 
-con = None
-try:
-	con = sql.connect('tux.db')
-	cur = con.cursor()
-except sql.Error:
-	file = open('tux.db','w+')
-	con = sql.connect('tux.db')
-	cur = con.cursor()
-	print("ln 16: TEST")
+def dbConnect():
+	'''
+	Any database accesses with require this series of try/excepts
+	'''
+	con = None
+	try:
+		con = sql.connect('tux.db')
+		cur = con.cursor()
+	except sql.Error:
+		file = open('tux.db', 'w+')
+		con = sql.connect('tux.db')
+		print("ln 16: TEST")
+	try:
+		cur.execute('SELECT * FROM tux.sqlite_master WHERE type = "table";')
+		print("ln 19: TEST")
+		data = cur.fetchall()
+		print(data)
+	except sql.OperationalError:
+		cur.executescript("""
+						CREATE TABLE Events(EventId INT, EventType TEXT, EventDate);
+						CREATE TABLE Types(Abbv TEXT, Full TEXT);
+						INSERT INTO Types VALUES('IO','Input Okay');
+						INSERT INTO Types VALUES('IF', 'Input Fail');
+						INSERT INTO Types Values('RO','RSS Okay');
+						INSERT INTO Types Values('RF','RSS Fail');
+						INSERT INTO Types Values('EO','Email Okay');
+						INSERT INTO Types Values('EF','Email Fail');
+						""")
+	return con
 
-try:
-	cur.execute('SELECT * FROM tux.sqlite_master WHERE type = "table";')
-	print("ln 19: TEST")
-	data = cur.fetchall()
-	print(data)
-except sql.OperationalError:
-	cur.executescript("""
-					CREATE TABLE Events(EventId INT, EventType TEXT, EventDate);
-					CREATE TABLE Types(Abbv TEXT, Full TEXT);
-					INSERT INTO Types VALUES('IO','Input Okay');
-					INSERT INTO Types VALUES('IF', 'Input Fail');
-					INSERT INTO Types Values('RO','RSS Okay');
-					INSERT INTO Types Values('RF','RSS Fail');
-					INSERT INTO Types Values('EO','Email Okay');
-					INSERT INTO Types Values('EF','Email Fail');
-					""")
-finally:
-	cur.close()
+
+def insertEvent(table, eventType, date):
+	if(eventType not in ['IO', 'IF', 'RO', 'RF'. 'EO', 'EF']):
+		print("That event type is not valid.")
+		return -1
+	con = dbConnect()
+	try:
+		id = 0
+		while(True):
+			id = random.random()
+			keys = cur.execute('SELECT * FROM table WHERE EventId = id')
+			if(id not in keys):
+				break
+		cur = con.cursor()
+		cur.execute("INSERT INTO table VALUES(id, eventType, date)")
+	con.close()
--- tux_email.py
+++ tux_email.py
@@ -5,7 +5,6 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-
 class tuxEmail:
 	'''
 	Email the user the RSS title & write event status to database
@@ -14,8 +13,8 @@ class tuxEmail:
 		self.emailAddress = emailAddress
 		self.rssTitle = rssTitle
 
-	def sendEmail(self, emailAddress):
-		emailAddress = input("Enter the recipient email address:\t")
+	def sendEmail(self, emailAddress, tuxPayload):
+		emailAddress = input("Enter the recipient email address: ")
 		print(emailAddress)
 		s = smtplib.SMTP('smtp.psu.edu')
 		msg = MIMEText('Content of email')
